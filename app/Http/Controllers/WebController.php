f28de27a8b27495eb13fc8781a8093d865288df5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 10:16:15 +0900

    add inde action & web/index.blade.php

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    //
+    public function index() 
+    {
+        return view('web.index');
+    }
 }
---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Web#Index</h1>
+
+@endsection
