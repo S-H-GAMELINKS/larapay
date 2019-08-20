0e187c27c4a4d871e029657d6afce3f9283bcc47
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 18:24:40 +0900

    create customer

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -24,8 +24,16 @@ class WebController extends Controller
 
     public function set_token(Request $request)
     {
+
+        $pay_jp_secret = env('MIX_PAYJP_SECRET_KEY');
+        \Payjp\Payjp::setApiKey($pay_jp_secret);
+
+        $customer = \Payjp\Customer::create(array(
+            "card" => request('token')
+        ));
+
         $user = Auth::user();
-        $user->token = request('token');
+        $user->token = $customer->id;
         $user->save();
 
         return response()->json($user);
