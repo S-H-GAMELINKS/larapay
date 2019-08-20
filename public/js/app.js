ff702d69f20ac29a1472e24231826803de4b2bab
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 14:02:32 +0900

    fix post token

--- public/js/app.js
+++ public/js/app.js
@@ -1904,9 +1904,7 @@ __webpack_require__.r(__webpack_exports__);
       axios__WEBPACK_IMPORTED_MODULE_1___default.a.defaults.headers["X-CSRF-TOKEN"] = jquery__WEBPACK_IMPORTED_MODULE_2___default()("meta[name=csrf-token]").attr("content");
       axios__WEBPACK_IMPORTED_MODULE_1___default.a.defaults.headers["content-type"] = "application/json";
       axios__WEBPACK_IMPORTED_MODULE_1___default.a.post("/set_token", {
-        user: {
-          token: token
-        }
+        token: token
       }).then(function (response) {
         console.log(response);
         alert("Success!");
