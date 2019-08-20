ac1255cfa37d34219648670069d8af0637ccfcf8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 11:15:23 +0900

    add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 bg-white fixed-bottom border-top border-secondary">
+    <div class="d-flex justify-content-center">
+        <span class="text-muted">Copyright © 2019 S.H. All Rights Reserved.</span>
+    </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,6 +20,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
 
@@ -30,6 +32,10 @@
                 @yield('content')
             </main>
         </div>
+
+        @component('components.footer')
+        @endcomponent
     </div>
 </body>
+
 </html>
