19b33d5131e44ccd859ffc73fa09a64c84ab000e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 10:41:53 +0900

    add container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,9 +72,11 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container">
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
     </div>
 </body>
 </html>
