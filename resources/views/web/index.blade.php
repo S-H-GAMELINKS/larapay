dd0a3e1b9f2a466898b41ff0fac18a6b72ad1a22
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 10:43:45 +0900

    add jumbotron for web/index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 
-<h1>Web#Index</h1>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Laravel Pay</h1>
+    <p class="lead">Developed by PHP/Laravel/Vue.js/PAY.JP API</p>
+  </div>
+</div>
 
 @endsection
